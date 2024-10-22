c793ac8fbb8c199effb842f2392c6dc8afbafc9b
JulianGaray16 <juliangaray.2116@gmail.com>
2024-10-22 00:12:03 -0300

    arranque la primera seccion de la rama sobreNosotros

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import "./App.css";
+import SobreNosotros from "./components/pages/SobreNosotros";
 function App() {
   return <>
   <h1>OASIS URBANO</h1>
+  <SobreNosotros></SobreNosotros>
   </>;
 }
 
---
+++ src/components/pages/SobreNosotros.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../estilos/nosotros.css'
+
+const SobreNosotros = () => {
+    return (
+        <>
+        <section className="container-fluid text-center bg-s1-nosotros">
+      <h1 className="text-black fw-bolder">SOBRE NOSOTROS</h1>
+      <div className="container">
+      <p className="lead fs-3 text-black">
+        El éxito de <b>Oasis Urbano</b> nace del sueño de ofrecer una experiencia
+        única, donde la hospitalidad y el confort se fusionan con el lujo y
+        detalle. Nuestros fundadores, comparten una pasión por la excelencia en 
+        el servicio y la creación de espacios que invitan a relajarse y
+        disfrutar de momentos inolvidables.
+      </p>
+      </div>
+      <div className="w-100 container">
+        <img
+          src=""
+          alt=""
+          className="w-100 img-fluid banner1-nosotros rounded"
+        />
+      </div>
+    </section>
+        </>
+    );
+};
+
+export default SobreNosotros;
