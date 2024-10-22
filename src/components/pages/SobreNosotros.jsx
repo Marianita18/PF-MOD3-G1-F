29d48da91a5299b9d4c2e0b7c5b510b4a390d157
JulianGaray16 <juliangaray.2116@gmail.com>
2024-10-22 14:44:07 -0300

    terminamos la seccion 2 y agregamos detalles de background a la seccion 1

--- src/components/pages/SobreNosotros.jsx
+++ src/components/pages/SobreNosotros.jsx
@@ -1,32 +1,52 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import '../estilos/nosotros.css'
-import banner1 from '../imgNosotros/banner1.jpg'
+import "../estilos/nosotros.css";
+import banner1 from "../imgNosotros/banner1.jpg";
+import banner2 from "../imgNosotros/banner2.jpg";
 
 const SobreNosotros = () => {
-    return (
-        <>
-        <section className="container-fluid text-center bg-s1-nosotros">
-      <h1 className="text-black fw-bolder">SOBRE NOSOTROS</h1>
-      <div className="container">
-      <p className="lead fs-3 text-black">
-        El éxito de <b>Oasis Urbano</b> nace del sueño de ofrecer una experiencia
-        única, donde la hospitalidad y el confort se fusionan con el lujo y
-        detalle. Nuestros fundadores, comparten una pasión por la excelencia en 
-        el servicio y la creación de espacios que invitan a relajarse y
-        disfrutar de momentos inolvidables.
-      </p>
-      </div>
-      <div className="w-100 container">
-        <img
-          src={banner1}
-          alt=""
-          className="w-100 img-fluid banner1-nosotros rounded"
-        />
-      </div>
-    </section>
-        </>
-    );
+  return (
+    <>
+      <section className="container-fluid text-center bg-s1-nosotros">
+        <h1 className="text-black fw-bolder">SOBRE NOSOTROS</h1>
+        <div className="container">
+          <p className="lead fs-3 text-black">
+            El éxito de <b>Oasis Urbano</b> nace del sueño de ofrecer una
+            experiencia única, donde la hospitalidad y el confort se fusionan
+            con el lujo y detalle. Nuestros fundadores, comparten una pasión por
+            la excelencia en el servicio y la creación de espacios que invitan a
+            relajarse y disfrutar de momentos inolvidables.
+          </p>
+        </div>
+        <div className="w-100 container">
+          <img
+            src={banner1}
+            alt=""
+            className="w-100 img-fluid banner1-nosotros rounded"
+          />
+        </div>
+      </section>
+      <section className="container-fluid bg-s2-nosotros">
+        <div className="row">
+          <div className="col-sm-12 col-lg-6">
+            <img
+              src={banner2}
+              alt=""
+              className="w-100 rounded banner2-nosotros"
+            />
+          </div>
+          <div className="col-sm-12 col-lg-6 lead fs-4">
+            Con una visión clara de redefinir la experiencia hotelera, unieron
+            su amor por los viajes y su experiencia en el sector de la
+            hospitalidad para crear un hotel que es mucho más que un lugar para
+            hospedarse. Desde la exquisita atención al cliente hasta el diseño
+            cuidado de cada rincón, su compromiso ha sido garantizar que cada
+            huésped se sienta como en casa, pero rodeado de lujo y confort.
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default SobreNosotros;
