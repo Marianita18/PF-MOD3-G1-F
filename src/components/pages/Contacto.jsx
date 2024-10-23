7574e3f5dc0af952f76a6143185529a48022305e
Eugenia Sansierra <eugesans@gmail.com>
2024-10-23 17:28:52 -0300

    se creo carpeta de estilos para seccion contacto, se agrego banner de contacto con titulo de la seccion

--- src/components/pages/Contacto.jsx
+++ src/components/pages/Contacto.jsx
@@ -1,5 +1,27 @@
+import { Container } from "react-bootstrap";
+import "../pages/styles/seccionContacto.css"
+
 export default function Contacto() {
   return (
-    <div>Contacto</div>
+    
+       <section className="mainSectionContacto">
+      <div className="bannerContacto">
+        <img
+          src="https://images.pexels.com/photos/3768095/pexels-photo-3768095.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="imagen de contacto"
+          className="bannerContactoImg"
+        />
+        <Container className="text-center overlay">
+          <h1
+            className="tituloSeccionContacto"
+          >
+            Contacto
+          </h1>
+        </Container>
+      </div>
+    
+    </section>
   )
 }
+ 
+ 
