dd4e2fa996bd6be3558a6a881f142e56abce9a92
Eugenia Sansierra <eugesans@gmail.com>
2024-10-23 18:56:01 -0300

    se agrego a la hoja de estilos !important porque prevalecia la funcion de bootstrap

--- src/components/pages/Contacto.jsx
+++ src/components/pages/Contacto.jsx
@@ -36,27 +36,27 @@ export default function Contacto() {
           </div>
           <div className="col-md-6">
             <h2 className="text-center">CONTÁCTANOS</h2>
-            <p className="text-center fst-italic fw-light">- No dudes en consultar nuestra disponibilidad y servicios para ofrecer -</p>
-            <FormularioContacto></FormularioContacto>
+            <p className="text-center fst-italic fw-light">- No dudes en consultar nuestra disponibilidad y servicios para ofrecer -</p> 
+             <FormularioContacto></FormularioContacto>
           </div>
         </div>
       </section>
       <div className=" container-fluid seccionContactoMapa p-3">
-        <h2 className="text-center">¡Vení a Visitarnos!</h2>
-        <p className=" fw-light mx-5 text-center ">
+     <h2 className="text-center">¡Vení a Visitarnos!</h2>
+     <p className=" fw-light mx-5 text-center ">
           Nos encontramos en el corazón del centro de Tucumán, a pocos pasos de
           los lugares más emblemáticos y llenos de historia que la provincia
           tiene para ofrecer. Disfruta de la comodidad y cercanía mientras
           exploras los destinos más destacados.
         </p>
-        <iframe
-          className=" w-100 p-3"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.1060679493985!2d-65.20974192366222!3d-26.83657849002869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d3ad7f30f1d%3A0xf8606cd659b8e3e4!2sRollingCode%20School!5e0!3m2!1ses-419!2sar!4v1717700548290!5m2!1ses-419!2sar"
-          height="500"
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-        ></iframe>
-        </div>
+     <iframe
+              className="w-100 mt-2"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.1060679493985!2d-65.20974192366222!3d-26.83657849002869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d3ad7f30f1d%3A0xf8606cd659b8e3e4!2sRollingCode%20School!5e0!3m2!1ses-419!2sar!4v1717700548290!5m2!1ses-419!2sar"
+              height="400"
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+     </div>
     </section>
   );
 }
