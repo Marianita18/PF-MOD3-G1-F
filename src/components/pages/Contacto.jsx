a579e03d8f371015924fa6b3731b9bf91a9cd27f
Eugenia Sansierra <eugesans@gmail.com>
2024-10-23 17:45:00 -0300

    se agrego link de fuente de google a index,tambien seccion informacion del hotel

--- src/components/pages/Contacto.jsx
+++ src/components/pages/Contacto.jsx
@@ -19,6 +19,21 @@ export default function Contacto() {
           </h1>
         </Container>
       </div>
+      <section className="container-fluid  seccionContactoInfo p-3">
+        <div className="row"> 
+          <div className="col-md-6 my-5">
+          <h3>Hotel Oasis Urbano </h3>
+          <p className="lead"><i class="bi bi-geo-alt"></i> Av. General Paz 576, Piso 9 Oficina 2, Tucumán, Argentina.</p>
+          <hr />
+          <p><i class="bi bi-phone"></i> <strong> Teléfono:</strong><span className="lead"> +54 9 381-5783030</span></p>
+          <p><i class="bi bi-envelope"></i> <strong> E-mail:</strong><span className="lead"> info@hoteloasisurbano.com</span></p>
+        </div>
+        <div className="col-md-6">
+          <h2 className="text-center">CONTÁCTANOS</h2>
+          <p>Aqui va el formulario</p>
+        </div>
+        </div>
+            </section>
     
     </section>
   )
