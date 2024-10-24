e57a64ccd6076fae0920bc10377ec9b89f872e07
JulianGaray16 <juliangaray.2116@gmail.com>
2024-10-24 14:02:42 -0300

    saque el componente sobreNosotros de app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
 import "./App.css";
-import SobreNosotros from "./components/pages/SobreNosotros.jsx";
 function App() {
   return <>
-  <SobreNosotros></SobreNosotros>
   </>;
 }
 
