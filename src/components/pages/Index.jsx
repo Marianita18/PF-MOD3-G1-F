7e90ec49c933d351b5827b0930db78e608ec0868
ismael <joseismaelvargas@users.noreply.github.com>
2024-10-30 14:44:19 -0300

    Se agrego Restaurante

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -1,12 +1,13 @@
 import { Reservas } from "./reserva/reserva"
 import { Info } from "./componentesindex/Info"
+import { Restaurante } from "./componentesindex/Restaurante"
 // import { Carrusel } from "./componentesIndex/Carrusel"
 export default function Index() {
   return (
     <> 
     {/* <Carrusel></Carrusel> */}
     <Info></Info>
-   
+   <Restaurante></Restaurante>
 
     </>
   )
