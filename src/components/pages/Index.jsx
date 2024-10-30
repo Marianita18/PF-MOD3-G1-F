5e5d26b280e4e537b51f008ac1b37cb4640e2fb9
ismael <joseismaelvargas@users.noreply.github.com>
2024-10-30 14:56:14 -0300

    Se agragaron estilos alos testimonios

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -1,13 +1,16 @@
 import { Reservas } from "./reserva/reserva"
 import { Info } from "./componentesindex/Info"
 import { Restaurante } from "./componentesindex/Restaurante"
+import { Testimonios } from "./componentesIndex/Testimonios"
 // import { Carrusel } from "./componentesIndex/Carrusel"
+
 export default function Index() {
   return (
     <> 
     {/* <Carrusel></Carrusel> */}
     <Info></Info>
    <Restaurante></Restaurante>
+   <Testimonios></Testimonios>
 
     </>
   )
