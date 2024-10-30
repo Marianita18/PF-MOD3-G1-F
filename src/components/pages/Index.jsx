4d542a21d16db82cab9dbdf3c77dbce30f4ad721
ismael <joseismaelvargas@users.noreply.github.com>
2024-10-30 14:58:54 -0300

    Se deployeo

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -1,7 +1,7 @@
 import { Reservas } from "./reserva/reserva"
 import { Info } from "./componentesindex/Info"
 import { Restaurante } from "./componentesindex/Restaurante"
-import { Testimonios } from "./componentesIndex/Testimonios"
+// import { Testimonios } from "./componentesIndex/Testimonios"
 // import { Carrusel } from "./componentesIndex/Carrusel"
 
 export default function Index() {
@@ -10,7 +10,7 @@ export default function Index() {
     {/* <Carrusel></Carrusel> */}
     <Info></Info>
    <Restaurante></Restaurante>
-   <Testimonios></Testimonios>
+   {/* <Testimonios></Testimonios> */}
 
     </>
   )
